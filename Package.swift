f9ac1ce5d318683234e17eb3da8b9d9b16261e28
Darren Allen <2169457+darrenallen7@users.noreply.github.com>
2024-07-02 23:42:58 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/passepartoutvpn/openssl-apple/releases/download/3.2.107/openssl.xcframework.zip",
+            url: "https://github.com/darrenallen7/openssl-apple/releases/download/3.2.107/openssl.xcframework.zip",
             checksum: "05fabd8a86adb86bdcbf998653f2d1bac90c2fdb9c6b8728fdd366dd6b5832e7")
     ]
 )
