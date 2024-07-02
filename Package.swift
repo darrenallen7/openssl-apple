25475f8b45ca6b55d5dcd9dc28f3f0a918e49bbe
Darren Allen <2169457+darrenallen7@users.noreply.github.com>
2024-07-02 22:19:45 +0000

    OpenSSL 3.2.0 (7)

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         .binaryTarget(
             name: "openssl",
             url: "https://github.com/passepartoutvpn/openssl-apple/releases/download/3.2.107/openssl.xcframework.zip",
-            checksum: "6aa5b917eb03487978b5d6ab1c019127af4f67568bd1f793c5b58cf786a49399")
+            checksum: "05fabd8a86adb86bdcbf998653f2d1bac90c2fdb9c6b8728fdd366dd6b5832e7")
     ]
 )
