35a278d0907ee75f8ccf84f4c97eaa4f891bffef
Darren Allen <2169457+darrenallen7@users.noreply.github.com>
2024-07-04 14:00:21 +0100

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "openssl-apple",
-            targets: ["opensslWrapper"]),
+            targets: ["openssl", "opensslWrapper"]),
     ],
     dependencies: [
     ],
